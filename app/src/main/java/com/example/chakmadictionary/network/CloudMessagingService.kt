bcbdd0f754413a317b8031fd173ff89ed9abe502
Neevir <neevirchakma@gmail.com>
2020-11-29 01:07:34 +0530

    Created custom notification channel to handle push notifications from FCM

--- app/src/main/java/com/example/chakmadictionary/network/CloudMessagingService.kt
+++ app/src/main/java/com/example/chakmadictionary/network/CloudMessagingService.kt
@@ -1,40 +1,66 @@
 package com.example.chakmadictionary.network
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.ContentValues.TAG
+import android.content.Context
+import android.content.Intent
+import android.media.RingtoneManager
+import android.os.Build
 import android.util.Log
 import android.widget.Toast
+import androidx.core.app.NotificationCompat
 import com.example.chakmadictionary.R
+import com.example.chakmadictionary.main.MainActivity
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.messaging.FirebaseMessaging
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
 import timber.log.Timber
 
 class CloudMessagingService : FirebaseMessagingService() {
 
 
+    override fun onMessageReceived(p0: RemoteMessage) {
+        super.onMessageReceived(p0)
+        val notifi:RemoteMessage.Notification?=p0.notification
+        Timber.i(p0.notification?.title)
+        sendNotification(notifi?.title,notifi?.body)
+    }
 
 
-    /*override fun onNewToken(p0: String) {
-        super.onNewToken(p0)
+    fun sendNotification(title:String?,message:String?){
+
+        val intent=Intent(this,MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val pendingIntent= PendingIntent.getActivity(this,0,intent,PendingIntent.FLAG_ONE_SHOT)
+
+        val defaultSoundUri=RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+
+        val notificationBuilder=NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_chakma_dictionary_icon)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setSound(defaultSoundUri)
+                .setAutoCancel(true)
+                .setContentIntent(pendingIntent)
 
 
-        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
-            if (task.isSuccessful) {
-                Timber.w(task.exception, "Fetching FCM registration token failed")
-                return@OnCompleteListener
-            }
-            //GET THE FIREBASE CLOUD MESSAGING TOKEN
-            val token = task.result
+        val notificationManager=getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
-            // Log and toast
-            Timber.d(token)
-            Toast.makeText(baseContext, token, Toast.LENGTH_SHORT).show()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            val channel=NotificationChannel(CHANNEL_ID,"channel 1",NotificationManager.IMPORTANCE_DEFAULT)
+            notificationManager.createNotificationChannel(channel)
+        }
 
+        notificationManager.notify(0,notificationBuilder.build())
 
-        })
+    }
 
-
-    }*/
+    companion object {
+        const val CHANNEL_ID="com.chakmadictionary.notification.channel";
+    }
 
 
 }
